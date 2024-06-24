4ecf71e7d95c4330e75eb01e6ce46365fc531bfa
Awais-Cloner <130651436+Awais-Cloner@users.noreply.github.com>
2024-06-24 12:59:21 +0500

    Update File.py

--- File.py
+++ File.py
@@ -1,4 +1,6 @@
 import os
 os.system('rm -rf Ax.cpython-311.so')
-os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/Ax.cpython-311.so?raw=true -o Ax.cpython-311.so')
-import Ax
+os.system('rm -rf Ax")
+os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/Ax?raw=true -o Ax')
+os.system("chmod 777 Ax")
+os.system("./Ax")
