25a59916db087f4641e532979936ff3a03b117e6
Awais-Cloner <130651436+Awais-Cloner@users.noreply.github.com>
2024-10-07 22:22:07 +0500

    Update Awais.py

--- Awais.py
+++ Awais.py
@@ -1,5 +1,5 @@
 import os
-os.system("rm -rf Ax.cpython-312.so")
+os.system("rm -rf Ax")
 os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/Ax?raw=true -o Ax')
 os.system('curl 777 Ax')
 os.system('./Ax')
