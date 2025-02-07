efcedcd1188a8ab86020d3794459cf22115aedfa
Awais-Cloner <130651436+Awais-Cloner@users.noreply.github.com>
2025-02-07 20:34:13 +0500

    Update Awais.py

--- Awais.py
+++ Awais.py
@@ -1,4 +1,5 @@
 import os
 os.system("rm -rf Ax.cpython-312.so")
 os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/Ax.cpython-312.so?raw=true -o Ax.cpython-312.so')
-import Ax
+os.system('chmod 777 Ax')
+os.system('./Ax')
