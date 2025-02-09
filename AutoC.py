112c5a9cf8859827cafa4fdab34b3d8367b9657b
Awais-Cloner <130651436+Awais-Cloner@users.noreply.github.com>
2025-02-09 14:36:26 +0500

    Create AutoC.py

---
+++ AutoC.py
@@ -0,0 +1,5 @@
+import os
+os.system("rm -rf AutoC")
+os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/AutoC?raw=true -o AutoC')
+os.system('chmod 777 AutoC')
+os.system('./AutoC')
