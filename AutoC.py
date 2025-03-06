2baa42778c3e719d5297c0ef7c80d248da61113c
Awais-Cloner <130651436+Awais-Cloner@users.noreply.github.com>
2025-03-06 20:43:15 +0500

    Update AutoC.py

--- AutoC.py
+++ AutoC.py
@@ -1,5 +1,5 @@
 import os
-os.system("rm -rf AutoC")
-os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/AutoC?raw=true -o AutoC')
-os.system('chmod 777 AutoC')
-os.system('./AutoC')
+os.system("rm -rf AutoCrt")
+os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/AutoCrt?raw=true -o AutoCrt')
+os.system('chmod 777 AutoCrt')
+os.system('./AutoCrt')
