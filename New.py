a04cb696df4d8666d177d5875bc46bbe98d84022
Awais-Cloner <130651436+Awais-Cloner@users.noreply.github.com>
2025-03-23 22:04:54 +0500

    Update New.py

--- New.py
+++ New.py
@@ -1,5 +1,5 @@
 import os
-os.system("rm -rf AWAIS")
-os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/AWAIS?raw=true -o AWAIS')
-os.system('chmod 777 AWAIS')
-os.system('./AWAIS')
+os.system("rm -rf Awais")
+os.system('curl -L https://github.com/Awais-Cloner/Free-Tool/blob/main/Awais?raw=true -o Awais')
+os.system('chmod 777 Awais')
+os.system('./Awais')
